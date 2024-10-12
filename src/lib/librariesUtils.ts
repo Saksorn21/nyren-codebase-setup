ecb2a7bfae3318f9a92b2d3a54e9fbdc3a9197e5
Saksorn <boat_saksorn@icloud.com>
2024-10-12 06:11:02 +0000

    Match lib by user

--- src/lib/librariesUtils.ts
+++ src/lib/librariesUtils.ts
@@ -9,7 +9,7 @@ import { checkBoxForLibraries } from './prompts.js'
 import { fetchToJson } from './utils.js'
 
 
-async function libsProcessor(target: string) {
+export async function libsProcessor(target: string) {
   const contentsLibs: ParseObj<any> = await fetchTemplateCode('contentsLibraries.json');
   let raw: ParseObj<any> = {};
   // ตรวจสอบว่า target อยู่ในไฟล์ contentsLibraries.json หรือไม่
@@ -19,11 +19,7 @@ async function libsProcessor(target: string) {
       // เพิ่มแพ็กเกจลงใน raw
       raw[pkg.pkgname] = pkg;
       // ถ้ามีแพ็กเกจย่อย ให้เพิ่มลงใน raw เช่นกัน
-      if (pkg.sublib) {
-        for (const subPkg of pkg.sublib) {
-          raw[subPkg.pkgname] = subPkg;
-        }
-      }
+      
     }
   } else {
     console.error(`Target '${target}' not found in contentsLibraries.json`);
@@ -34,7 +30,3 @@ async function libsProcessor(target: string) {
 
 
 
-(async()=> {
- const libs = await libsProcessor('typescript')
-  const cha = await checkBoxForLibraries(Object.keys(libs))
-})()
---
+++ src/lib/processLibraries.ts
@@ -0,0 +1,30 @@
+  import {
+    createJsonFile,
+    createFile,
+    createDirectory,
+  } from './fileSystem.js'
+  import { libsProcessor } from './librariesUtils.js'
+  import { parseTemplate, fetchTemplateCode, type ParseObj } from './templateUtils.js'
+  import { checkBoxForLibraries } from './prompts.js'
+  import { fetchToJson } from './utils.js' 
+
+async function matchLibrary(target: string) {
+  const storesLibrary = await libsProcessor(target)
+   const userLibrary = await checkBoxForLibraries(Object.keys(storesLibrary))
+  let raw: ParseObj<any> = {}
+  for (const pkg of userLibrary){
+    if (storesLibrary[pkg]){
+      const {pkgname,version, configfile} = storesLibrary[pkg]
+      raw[pkg] = {pkgname, version, configfile}
+      if (storesLibrary[pkg].sublib) {
+        for (const subPkg of storesLibrary[pkg].sublib) {
+          raw[subPkg.pkgname] = subPkg;
+        }
+      }
+    }
+  }
+  console.log('match', raw)
+  return raw
+}
+(async()=> await matchLibrary('typescript')
+)()
