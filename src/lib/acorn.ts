c5daff2f9a7961e01b0f99846f4bbf75d5da9be8
Saksorn <boat_saksorn@icloud.com>
2024-11-06 16:26:11 +0000

    Debug message stderr

--- src/lib/acorn.ts
+++ src/lib/acorn.ts
@@ -1,12 +1,21 @@
-import  { Token, Parser, defaultOptions, getLineInfo, isIdentifierChar, isIdentifierStart, isNewLine, keywordTypes, lineBreak, lineBreakG, nonASCIIwhitespace, parse, parseExpressionAt, tokContexts, tokTypes, tokenizer, version } from 'acorn'
+import  { Token, Parser, defaultOptions, getLineInfo, isIdentifierChar, isIdentifierStart, isNewLine, keywordTypes, lineBreak, lineBreakG, nonASCIIwhitespace, parse, parseExpressionAt, tokContexts, tokTypes, tokenizer, version, parse } from 'acorn'
+import type { ecmaVersion, Options } from 'acorn'
 //ts-no-check
 import classFields from 'acorn-class-fields'
 import staticClassFeatures from 'acorn-static-class-features'
 import privateClassElements from 'acorn-private-class-elements'
-//ts-check
+
 import tsPlugin from 'acorn-typescript'
 
-const acorn = Parser.extend(tsPlugin() as any);
-const parseCode = (code: string, options: any) => tokenizer(code, options)
+const acorn = Parser
+.extend(tsPlugin())
+ // .extend(classFields)
+ .extend(staticClassFeatures)
+// .extend(privateClassElements)
+const parseCode = {
+  tokenizer: (code: string, options: Options) => tokenizer(code, options),
+  parse: (code: string, options: Options) => acorn.parse(code, options)
+  }
 export { tokTypes, getLineInfo, isIdentifierChar, isIdentifierStart, isNewLine, keywordTypes, lineBreak, lineBreakG, nonASCIIwhitespace, parse, parseExpressionAt, tokContexts,Token}
+export type { Options , ecmaVersion}
 export default parseCode
--- src/lib/utils/modifyStderr.ts
+++ src/lib/utils/modifyStderr.ts
@@ -1,10 +1,10 @@
 import process from 'node:process'
 import parseCode, { tokTypes, Token, isNewLine, keywordTypes, lineBreak, lineBreakG, nonASCIIwhitespace, parseExpressionAt, tokContexts,} from '../acorn.js'
 import { clearAnsiCodes } from './main.js'
-
+import type { ecmaVersion, Options } from 'acorn'
 import color from './color.js'
 import clone from './clone.js'
-
+import {full} from 'acorn-walk'
 
 const modifyStderr = (stderr: typeof process.stderr) => stderr.on('data', data => {
   let str = data.toString().split('\n')
@@ -19,40 +19,35 @@ const modifyStderr = (stderr: typeof process.stderr) => stderr.on('data', data =
   //console.log(str)
   
   
-  const optionsAcorn = {
-    ecmaVersion: 'latest',
+  const optionsAcorn: Options = {
+    ecmaVersion: 2022,
     sourceType: 'module',
     locations: true,
     preserveParens: true,
     allowAwaitOutsideFunction: true,
-
   }
   const code = `
-  interface Sos {
-    name: string;
-    age: number;
-    
-  }
   
-  const name: string = 'nyrenx';
-  class sos {
-  static pname: string = 'nyrenx';
-  #this.boat = 'boat'
-  constructor(public name: string){
-  this.name = name;
-  }
-  }
   
+  const ppp: string = 'nyrenx';
+  class sod{
+  #home = 'home'
+  namep: string = 'sod'
+  constructor(p: string){}
+  #hgg(){
+  return this.#home
+  }
+  }
   `
     try {
-    const ast: Token[] = [...parseCode(code,optionsAcorn)]
-    
-    console.log(ast);
-      return
+    const ast = [...parseCode.tokenizer(code,optionsAcorn)]
+   // const ast = full(parseCode.parse(code,optionsAcorn), node => console.log(node))
+    //console.log('ast',ast)
+      
       highlightSyntax(ast)
 
   } catch (error: unknown) {
-     //console.log(error)
+     console.log(error)
        cloneData.forEach((item: string, index: number) => {
          
          if (item.includes('^')) {
@@ -83,88 +78,99 @@ const modifyStderr = (stderr: typeof process.stderr) => stderr.on('data', data =
   // console.log(str.join('\n'))
 })
 const highlightSyntax = (ast: Token[]) => {
-  const outputSyntax: Array<string> = []
-    let currentLine = 1;  // เริ่มบรรทัดที่ 1
-    let currentColumn = 0;  // เริ่มคอลัมน์ที่ 0
+  const outputSyntax: Array<string> = [];
+  let currentLine = 1;  // เริ่มบรรทัดที่ 1
+  let currentColumn = 0;  // เริ่มคอลัมน์ที่ 0
+  let prevToken = null;
 
-    ast.forEach((token: any) => {
-        const { label } = token.type;
-        const { line: startLine, column: startColumn } = token.loc?.start;
-        const { line: endLine, column: endColumn } = token.loc?.end;
+  ast.forEach((token: any) => {
+    const { label } = token.type;
+    const { line: startLine, column: startColumn } = token.loc?.start;
+    const { line: endLine, column: endColumn } = token.loc?.end;
 
-        // แทรกการขึ้นบรรทัดใหม่หากบรรทัดเปลี่ยน
-        while (currentLine < startLine) {
-              outputSyntax.push('\n');
-            currentLine++;
-            currentColumn = 0;
-        }
+    // แทรกการขึ้นบรรทัดใหม่หากบรรทัดเปลี่ยน
+    while (currentLine < startLine) {
+      outputSyntax.push('\n');
+      currentLine++;
+      currentColumn = 0;
+    }
 
-        // แทรกช่องว่างเพื่อให้คอลัมน์ตรงกับต้นฉบับ
-        while (currentColumn < startColumn) {
-              outputSyntax.push(' ');
-            currentColumn++;
-        }
+    // แทรกช่องว่างเพื่อให้คอลัมน์ตรงกับต้นฉบับ
+    while (currentColumn < startColumn) {
+      outputSyntax.push(' ');
+      currentColumn++;
+    }
 
-        let highlighted = false;
+    let highlighted = false;
 
-        // ตรวจสอบโทเค็นใน tokTypes และ tokContexts
-        for (const [_, value] of Object.entries({ ...tokTypes, ...tokContexts })) {
-          const {label: tokLabel, token: tok } = value as any
-            if (tokLabel === token.value || tok === token.value) {
-                highlighted = true;
-                if (label === '^' && token.value !== undefined) {
-                  
-                      outputSyntax.push(color.hex('f14c4c')(token.value));
-                } else if (tok) {
-                  
-                      outputSyntax.push(color.green('"' + token.value + '"'));
-                } else {
-                  highlighted = false
-                      
-                }
-                break;
-            }
-        }
-      if (!highlighted && (token.value === '""' || token.value === "''")) {
-          outputSyntax.push(color.deepBlue(token.value));
+    // ตรวจสอบโทเค็นใน tokTypes และ tokContexts
+    for (const [_, value] of Object.entries({ ...tokTypes, ...tokContexts })) {
+      const { label: tokLabel, keyWord } = value as any;
+      if (tokLabel === token.value || keyWord === token.value) {
         highlighted = true;
+        if (label === '^' && token.value !== undefined) {
+          outputSyntax.push(color.hex('f14c4c')(token.value));
+        } else if (keyWord !== undefined && keyWord === token.type.keyword) {
+          outputSyntax.push(color.green('"' + token.value + '"'));
+        } else {
+          highlighted = false;
         }
+        break;
+      }
+    }
 
-        // ตรวจสอบการขึ้นบรรทัดใหม่และช่องว่าง non-ASCII
-        if (lineBreak.test(token.value)) {
-              outputSyntax.push('\n');
-            highlighted = true;
-        } else if (nonASCIIwhitespace.test(token.value)) {
-              outputSyntax.push(' ');
-            highlighted = true;
+    if (!highlighted && (token.value === '""' || token.value === "''")) {
+      outputSyntax.push(color.deepBlue(token.value));
+      highlighted = true;
+    }
+
+    // ตรวจสอบการขึ้นบรรทัดใหม่และช่องว่าง non-ASCII
+    if (lineBreak.test(token.value)) {
+      outputSyntax.push('\n');
+      highlighted = true;
+    } else if (nonASCIIwhitespace.test(token.value)) {
+      outputSyntax.push(' ');
+      highlighted = true;
+    }
+
+    // กำหนดสีสำหรับประเภทโทเค็นหลัก หากยังไม่ได้ไฮไลต์
+    if (!highlighted) {
+      if (keywordTypes[token.value]) {
+        outputSyntax.push(color.hex('A78CFA')(token.value));
+      } else if (label === 'privateId') {
+        if (prevToken && prevToken.type.label === '.') {
+          outputSyntax.push(color.hex('f14c4c')('#' + token.value));
+        } else {
+        outputSyntax.push(color.hex('6495EE')('#' + token.value));
+      }
+      } else if (label === 'name') {
+        // ตรวจสอบว่าโทเค็นก่อนหน้าเป็นจุด (.)
+        if (prevToken && prevToken.type.label === '.') {
+          outputSyntax.push(color.hex('f14c4c')(token.value));
+        } else {
+          outputSyntax.push(color.grey(token.value));
         }
+      } else if (label === 'string') {
+        outputSyntax.push(color.hex('98C379').visible('"' + token.value + '"'));
+      } else if (label === 'num') {
+        outputSyntax.push(color.amber.italic(token.value));
+      } else if (token.value === undefined) {
+        outputSyntax.push(color.chalk.green.bold(label));
+      } else {
+        outputSyntax.push(color.chalk.bold.visible(token.value));
+      }
+    }
 
-        // กำหนดสีสำหรับประเภทโทเค็นหลัก หากยังไม่ได้ไฮไลต์
-        if (!highlighted) {
-            if (keywordTypes[token.value]) {
-                  outputSyntax.push(color.hex('A78CFA')(token.value));
-            } else if (label === 'name') {
-                  outputSyntax.push(color.grey(token.value))
-            } else if (label === 'string') {
-              
-                  outputSyntax.push( color.lightSteelBlue('"' +token.value + '"'));
-            } else if (label === 'num') {
-                  outputSyntax.push(color.amber.italic(token.value) );
-            } else if (token.value === undefined) {
-                  outputSyntax.push(color.chalk.green.bold(label));
-            } else {
-              //console.log(token.value)
-              outputSyntax.push(color.chalk.bold.visible(token.value))  // ใช้สีปกติสำหรับโทเค็นอื่นๆ
-            }
-        }
+    // อัปเดตตำแหน่งล่าสุดตามตำแหน่งสิ้นสุดของโทเค็นนี้
+    currentLine = endLine;
+    currentColumn = endColumn;
 
-        // อัปเดตตำแหน่งล่าสุดตามตำแหน่งสิ้นสุดของโทเค็นนี้
-        currentLine = endLine;
-        currentColumn = endColumn;
-    });
-  //process.stdout.write(outputSyntax.join(''))
-errorMessageAndPaths(outputSyntax)
-      // เพิ่มบรรทัดใหม่หลังจากจบการแสดงผล
+    // อัปเดต prevToken ให้เป็นโทเค็นปัจจุบัน
+    prevToken = token;
+  });
+
+  // แสดงผล
+  errorMessageAndPaths(outputSyntax);
 };
 const atPath = (path: string) => { 
   const match = path.match(/at\s+([^\s]+)?\s?([^\s:]+):(\d+):(\d+)/);
