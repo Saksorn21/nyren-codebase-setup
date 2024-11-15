29553c1a6f9d6dbc57ed7b7a190b6c580ca408eb
Saksorn <boat_saksorn@icloud.com>
2024-11-15 09:58:33 +0000

    Edit clearAnsi

--- src/lib/utils/clearAnsi.ts
+++ src/lib/utils/clearAnsi.ts
@@ -1,8 +1,7 @@
 import ansiRegex from 'ansi-regex'
- const clearAnsiCodes = (str: string): string =>
+export default (str: string): string =>
   typeof str === 'string'
     ? (() => str.replace(ansiRegex(), ''))()
     : (() => {
         throw new TypeError(`Expected a 'string', got '${typeof str}'`)
       })()
-export default clearAnsiCodes
