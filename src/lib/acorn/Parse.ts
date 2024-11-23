e11747a6480cafc9bf3f9c5810e29f616997e4ac
Saksorn <boat_saksorn@icloud.com>
2024-11-23 15:04:01 +0000

    Test Parser

--- src/lib/acorn/Parse.ts
+++ src/lib/acorn/Parse.ts
@@ -1,270 +1,362 @@
 import kw from './schema-keywordType.js'
 class SourcePosition {
-  constructor(public line: number, public column: number) {}
+  constructor(
+    public line: number,
+    public column: number
+  ) {}
 }
 
 class SourceLocation {
-  constructor(public start: SourcePosition, public end: SourcePosition) {}
+  constructor(
+    public start: SourcePosition,
+    public end: SourcePosition
+  ) {}
 }
 
 class TokenType {
-  constructor(public label: string, public keyword: string = '') {}
+  constructor(
+    public label: string,
+    public keyword: string = ''
+  ) {}
 }
 
 class Token {
   constructor(
-      public type: TokenType,
-      public value: any,
-      public start: number,
-      public end: number,
-      public loc: SourceLocation
+    public type: TokenType,
+    public value: any,
+    public start: number,
+    public end: number,
+    public loc: SourceLocation
   ) {}
 }
 export default class Tokenizer {
-    private position: number = 0;
-    private line: number = 1;
-    private column: number = 0;
-   
-    private contextTokens: Set<string> = new Set([
-        '.', ',', '=', '(', '{', '<', '>', '}', '[', ']', '${', ';', '===', '<=', '>=', '+=', '-=', '*=', '/=', '&&', '||'
-    ]);
- private input: string
-    public keyword = kw.keywordAnyTypes
-    constructor() {
-      this.input = ''
-    }
-  public parse(input: string){
-    this.input = input
+  private position: number = 0
+  private line: number = 1
+  private column: number = 0
+
+  private contextTokens: Set<string> = new Set([
+    '.',
+    ',',
+    '=',
+    '(',
+    '{',
+    '<',
+    '>',
+    '}',
+    '[',
+    ']',
+    '${',
+    ';',
+    '===',
+    '<=',
+    '>=',
+    '+=',
+    '-=',
+    '*=',
+    '/=',
+    '&&',
+    '||',
+  ])
+
+  public keyword = kw.keywordAnyTypes
+
+  constructor(private input: string) {}
+
+  private isContextToken(value: string): boolean {
+    return this.contextTokens.has(value)
   }
-    private isContextToken(value: string): boolean {
-        return this.contextTokens.has(value);
-    }
 
-    private readWord(): { value: string; start: number; end: number } | null {
-        let start = this.position;
-        let currentWord = '';
+  private readWord(): { value: string; start: number; end: number } | null {
+    let start = this.position
+    let currentWord = ''
 
-        while (this.position < this.input.length) {
-            const char = this.input[this.position];
-            const isSeparator = /\s|,|\(|\)|{|}|\n/.test(char);
+    while (this.position < this.input.length) {
+      const char = this.input[this.position]
+      const isSeparator = /\s|,|\(|\)|{|}|\n/.test(char)
 
-            if (isSeparator || this.isContextToken(char)) {
-                if (currentWord) {
-                    // Found a word, return it
-                    return { value: currentWord, start, end: this.position };
-                }
+      // Update line and column when new line is encountered
+      if (char === '\n') {
+        this.line++
+        this.column = 0
+      }
 
-                if (char.trim()) {
-                    // Return single-character context token (e.g., `=` or `(`)
-                    return { value: char, start: this.position, end: this.position + 1 };
-                }
-
-                start = ++this.position; // Skip separator
-                continue;
-            }
-
-            currentWord += char;
-            this.position++;
-        }
-
+      if (isSeparator || this.isContextToken(char)) {
         if (currentWord) {
-            // Return last word at the end of input
-            return { value: currentWord, start, end: this.input.length };
+          return { value: currentWord, start, end: this.position }
         }
 
-        return null; // No more tokens
-    }
-
-    private createToken(value: string, start: number, end: number): Token {
-        const loc = new SourceLocation(
-            new SourcePosition(this.line, this.column - (end - start)),
-            new SourcePosition(this.line, this.column)
-        );
-
-        if (this.isContextToken(value)) {
-            return this.readContextToken(value, start, end, loc);
-        } else if (this.keyword.includes(value)) {
-            return this.readKeywordToken(value, start, end, loc);
-        } else if (/^["'].*["']$/.test(value)) {
-            return this.readStringToken(value, start, end, loc);
-        } else if (!isNaN(Number(value))) {
-            return this.readNumberToken(value, start, end, loc);
-        } else if (/^#\w+$/.test(value)) {
-            return this.readPrivateIdentifierToken(value, start, end, loc);
-        } else if (/^\/.*\/[gimsuy]*$/.test(value)) {
-            return this.readRegExpToken(value, start, end, loc);
-        } else if (/^`.*`$/.test(value)) {
-            return this.readTemplateToken(value, start, end, loc);
-        } else if (/^[a-zA-Z_$][a-zA-Z0-9_$]*$/.test(value)) {
-            return this.readIdentifierToken(value, start, end, loc);
-        } else {
-            return this.readSymbolToken(value, start, end, loc);
-        }
-    }
-
-    public getTokens(): Token[] {
-        const tokens: Token[] = [];
-
-        while (this.position < this.input.length) {
-            const rawToken = this.readWord();
-            if (!rawToken) break;
-
-            const { value, start, end } = rawToken;
-
-            // Handle line breaks (if any)
-            if (value === '\n') {
-                this.line++;
-                this.column = 0;
-                continue;
-            }
-
-            const token = this.createToken(value, start, end);
-            tokens.push(token);
-        }
-
-        return tokens;
-    }
-
-    public [Symbol.iterator]() {
-        const tokens = this.getTokens();
-        let index = 0;
-
-        return {
-            next: (): IteratorResult<Token> => {
-                if (index < tokens.length) {
-                    return { value: tokens[index++], done: false };
-                }
-                return { value: null as any, done: true };
-            },
-        };
-    }
-
-    private readContextToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('context', value);
-        return new Token(type, value, start, end, loc);
-    }
-
-    private readKeywordToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('keyword', value);
-        return new Token(type, value, start, end, loc);
-    }
-
-    private readStringToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('string');
-        return new Token(type, value, start, end, loc);
-    }
-
-    private readNumberToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('num');
-        let numValue: number | bigint;
-
-        if (value.endsWith('n')) {
-            numValue = BigInt(value.slice(0, -1)); 
-        } else {
-
-            numValue = parseFloat(value);
-
-            if (isNaN(numValue)) {
-                throw new Error(`Invalid number: ${value}`);
-            }
-        }
-
-        return new Token(type, numValue, start, end, loc);
-    }
-
-    private readPrivateIdentifierToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('privateId');
-        return new Token(type, value, start, end, loc);
-    }
-
-    private readRegExpToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const match = /^\/(.*?)\/([gimsuy]*)$/.exec(value);
-        if (!match) throw new Error(`Invalid RegExp: ${value}`);
-        const type = new TokenType('regexp');
-        return new Token(
-            type,
-            { pattern: match[1], flags: match[2], value: new RegExp(match[1], match[2]) },
+        if (char.trim()) {
+          // Return single-character token
+          this.position++ // Move position forward
+          return {
+            value: char,
             start,
-            end,
-            loc
-        );
+            end: this.position,
+          }
+        }
+
+        // Skip whitespace or separators
+        this.position++
+        start = this.position
+        continue
+      }
+
+      currentWord += char
+      this.position++
+      this.column++
     }
 
-    private readTemplateToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('template');
-        return new Token(type, value, start, end, loc);
+    if (currentWord) {
+      // Return last word at the end of input
+      return { value: currentWord, start, end: this.input.length }
     }
 
-    private readIdentifierToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('identifier');
-        return new Token(type, value, start, end, loc);
+    return null // No more tokens
+  }
+
+  private createToken(value: string, start: number, end: number): Token {
+    const loc = new SourceLocation(
+      new SourcePosition(this.line, this.column - (end - start)),
+      new SourcePosition(this.line, this.column)
+    )
+
+    if (this.isContextToken(value)) {
+      return this.readContextToken(value, start, end, loc)
+    } else if (this.keyword.includes(value)) {
+      return this.readKeywordToken(value, start, end, loc)
+    } else if (/^["'].*["']$/.test(value)) {
+      return this.readStringToken(value, start, end, loc)
+    } else if (!isNaN(Number(value))) {
+      return this.readNumberToken(value, start, end, loc)
+    } else if (/^#\w+$/.test(value)) {
+      return this.readPrivateIdentifierToken(value, start, end, loc)
+    } else if (/^\/.*\/[gimsuy]*$/.test(value)) {
+      return this.readRegExpToken(value, start, end, loc)
+    } else if (/^`.*`$/.test(value)) {
+      return this.readTemplateToken(value, start, end, loc)
+    } else if (/^[a-zA-Z_$][a-zA-Z0-9_$]*$/.test(value)) {
+      return this.readIdentifierToken(value, start, end, loc)
+    } else {
+      return this.readSymbolToken(value, start, end, loc)
+    }
+  }
+
+  public getTokens(): Token[] {
+    const tokens: Token[] = []
+
+    while (this.position < this.input.length) {
+      const rawToken = this.readWord()
+      if (!rawToken) break
+
+      const { value, start, end } = rawToken
+
+      // Handle line breaks (if any)
+      if (value === '\n') {
+        this.line++
+        this.column = 0
+        continue
+      }
+
+      const token = this.createToken(value, start, end)
+      tokens.push(token)
     }
 
-    private readSymbolToken(value: string, start: number, end: number, loc: SourceLocation): Token {
-        const type = new TokenType('symbol');
-        return new Token(type, value, start, end, loc);
+    return tokens
+  }
+
+  public [Symbol.iterator]() {
+    const tokens = this.getTokens()
+    let index = 0
+
+    return {
+      next: (): IteratorResult<Token> => {
+        if (index < tokens.length) {
+          return { value: tokens[index++], done: false }
+        }
+        return { value: null as any, done: true }
+      },
     }
+  }
+
+  private readContextToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('context', value)
+    return new Token(type, value, start, end, loc)
+  }
+
+  private readKeywordToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('keyword', value)
+    return new Token(type, value, start, end, loc)
+  }
+
+  private readStringToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('string')
+    return new Token(type, value, start, end, loc)
+  }
+
+  private readNumberToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('num')
+    let numValue: number | bigint
+
+    if (value.endsWith('n')) {
+      numValue = BigInt(value.slice(0, -1))
+    } else {
+      numValue = parseFloat(value)
+
+      if (isNaN(numValue)) {
+        throw new Error(`Invalid number: ${value}`)
+      }
+    }
+
+    return new Token(type, numValue, start, end, loc)
+  }
+
+  private readPrivateIdentifierToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('privateId')
+    return new Token(type, value, start, end, loc)
+  }
+
+  private readRegExpToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const match = /^\/(.*?)\/([gimsuy]*)$/.exec(value)
+    if (!match) throw new Error(`Invalid RegExp: ${value}`)
+    const type = new TokenType('regexp')
+    return new Token(
+      type,
+      {
+        pattern: match[1],
+        flags: match[2],
+        value: new RegExp(match[1], match[2]),
+      },
+      start,
+      end,
+      loc
+    )
+  }
+
+  private readTemplateToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('template')
+    return new Token(type, value, start, end, loc)
+  }
+
+  private readIdentifierToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('identifier')
+    return new Token(type, value, start, end, loc)
+  }
+
+  private readSymbolToken(
+    value: string,
+    start: number,
+    end: number,
+    loc: SourceLocation
+  ): Token {
+    const type = new TokenType('symbol')
+    return new Token(type, value, start, end, loc)
+  }
 }
 export class Parser {
-  pos: number = 0;
-  tokens: Token[] = [];
-  eof: Token;
+  pos: number = 0
+  tokens: Token[] = []
+  eof: Token
 
   constructor(private readonly code: string) {
-    this.eof = new Token({ type: 'eof', label: 'eof', keyword: 'eof', value: null });
+    this.eof = new Token({
+      type: 'eof',
+      label: 'eof',
+      keyword: 'eof',
+      value: null,
+    })
   }
 
   parse() {
-    const arr = this.code.split(' ');
+    const arr = this.code.split(' ')
 
     for (let inCode of arr) {
-      let label = '';
+      let label = ''
       let keyword: string | undefined = undefined
-      let value: any = '';
+      let value: any = ''
       if (kw.keywordAnyTypes.includes(inCode)) {
-        label = inCode;
-        keyword = inCode;
-        value = inCode;
+        label = inCode
+        keyword = inCode
+        value = inCode
       } else {
         if (!isNaN(Number(inCode))) {
-          label = 'num';
+          label = 'num'
           keyword = undefined
-          value = Number(inCode);
+          value = Number(inCode)
         } else {
-          label = 'name';
-          keyword = inCode;
-          value = inCode;
+          label = 'name'
+          keyword = inCode
+          value = inCode
         }
       }
-      this.tokens.push(new Token({ type: 'identifier', label, keyword, value }));
+      this.tokens.push(new Token({ type: 'identifier', label, keyword, value }))
     }
 
-    this.tokens.push(this.eof); // Append EOF token at the end
+    this.tokens.push(this.eof) // Append EOF token at the end
   }
 
   getToken() {
-    this.next();
-    return this.tokens[this.pos];
+    this.next()
+    return this.tokens[this.pos]
   }
 
   next() {
     if (this.pos < this.tokens.length - 1) {
-      this.pos++;
+      this.pos++
     }
   }
 }
 
-const pp = Parser.prototype;
+const pp = Parser.prototype
 if (typeof Symbol !== 'undefined') {
-  (pp as any)[Symbol.iterator] = function () {
+  ;(pp as any)[Symbol.iterator] = function () {
     return {
       next: () => {
-        const token = this.getToken();
+        const token = this.getTokens()
         return {
           done: token.type === this.eof.type,
           value: token,
-        };
+        }
       },
-    };
-  };
+    }
+  }
 }
--- src/lib/utils/modifyStderr.ts
+++ src/lib/utils/modifyStderr.ts
@@ -9,7 +9,7 @@ import type { KeywordType, CustomToken } from '../acorn/labels/abstract.js'
 import types from '../acorn/schema-keywordType.js'
 import ErrorLogManager, { type ErrorAndPath } from '../acorn/ErrorLogManager.js'
 import Fusion from '../acorn/Fusion.js'
-import Parser from '../acorn/Parse.js'
+import Tokenizer from '../acorn/Parse.js'
 import clearAnsiCodes from '../utils/clearAnsi.js'
 const modifyStderr = (stderr: typeof process.stderr) =>
   stderr.on('data', data => {
@@ -80,8 +80,28 @@ console.log('true')
       debug(`Error caught: ${error.message}`)
      // console.log(error)
       console.log('error')
-      const parseError = processTokens(modifiedData.join('\n'))
-      console.log(parseError)
+      try {
+        console.log('start Tokenizer');
+        const tokenizer = new Tokenizer(modifiedData.join('\n'));
+
+        // ทดสอบ getTokens()
+        console.log('Tokens from getTokens:');
+        const tokens = tokenizer.getTokens();
+        tokens.forEach(token => {
+            console.log(`Type: ${token.type.label}, Value: ${token.value}, Start: ${token.start}, End: ${token.end}`);
+        });
+
+        // ทดสอบ Symbol.iterator
+        console.log('Tokens from Symbol.iterator:');
+        for (const token of tokenizer) {
+            console.log(`Type: ${token.type.label}, Value: ${token.value}, Start: ${token.start}, End: ${token.end}`);
+        }
+      } catch (error: unknown) {
+         console.error('Error parsing code:', error);
+      }
+      
+      
+      
       return
       modifiedData.forEach((item: string, index: number) => {
         if (item.includes('^')) {
@@ -117,65 +137,7 @@ console.log('true')
   })
 // color for syntax by. Eva Dark
 
-function processTokens(data: string) {
-  const tokens: string[] = [];
-  let currentToken = '';
-  let str = '';
 
-  // Loop through each character in the input string
-  for (let i = 0; i < data.length; i++) {
-      const char = data[i];
-      const nextChar = data[i + 1] ?? ''; // Lookahead for the next character
-
-      // Check for word boundaries or separators
-      if (/\s/.test(char) || char === ',' || char === '(' || char === ')' || char === '\n') {
-          if (currentToken) {
-              tokens.push(currentToken); // Push the current token
-              currentToken = ''; // Reset the token
-          }
-
-          // Handle the separators explicitly (e.g., include them as tokens)
-          if (char.trim() !== '') {
-              tokens.push(char);
-          }
-      } else {
-          currentToken += char; // Build the token
-      }
-  }
-
-  // If there's any remaining token at the end of the loop, add it
-  if (currentToken) tokens.push(currentToken);
-
-  // Process tokens with coloring logic
-  const coloredTokens = tokens.map((token, idx) => {
-      const next = tokens[idx + 1] ?? '';
-      const prev = tokens[idx - 1] ?? '';
-      const normal = token.replace(/[^\w\s]/gi, '');
-
-      if (types.keywordAnyTypes.includes(token)) {
-          return color.hex('C678DD')(token); // Keyword coloring
-      } else if (typeof token === 'string' && (token.includes('\'') || token.includes('\"'))) {
-          return color.hex('98C379')(token); // String coloring
-      } else if (!isNaN(Number(normal)) && parseInt(normal) && next !== '|') {
-          return color.hex('D19A66')(token); // Number coloring
-      } else if (next === '(') {
-          return color.hex('61AFEF')(token); // Function call coloring
-      } else if (token === ',') {
-          return color.hex('ABB2BF0')(token); // Separator coloring
-      } else {
-          return color.hex('ABB2BF0')(token); // Default coloring
-      }
-  });
-
-  // Combine tokens back into a single string
-  str = coloredTokens.join('');
-  return str;
-}
-
-// Usage
-const input = `const sum = (a, b) => a + b;`;
-const result = processTokens(input)
-console.log('process',result)
 process.stdout.on('data', data => console.log(data.toString()))
 process.stderr.on('data', data => console.log(data.toString()))
 export default modifyStderr
